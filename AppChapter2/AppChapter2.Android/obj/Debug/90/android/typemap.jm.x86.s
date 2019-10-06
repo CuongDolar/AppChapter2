6668b699909678e48eda6622bd9883ba90a0ac2c
CuongDolar <dinhcuong130698@gmail.com>
2019-10-06 16:58:56 +0700

    first commit

---
+++ AppChapter2/AppChapter2.Android/obj/Debug/90/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: d4d5f4507dbaf5542e25c1917d64780a27c1c407 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1343
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 351867
+	.include	"typemap.jm.inc"
---
+++ AppChapter2/AppChapter2/ContentPageExample.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppChapter2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContentPageExample : ContentPage
+    {
+        public ContentPageExample()
+        {
+            InitializeComponent();
+        }
+
+        
+        /*private async Task<bool> ButtonClicked(object sender, EventArgs e)
+        {
+            var b = sender as Button;
+            b.Text = " It is so cool";
+            return await Task.FromResult(true);
+        }*/
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            var a = sender as Button;
+            a.Text = "nguyen dinh cuong";
+            a.Text = a.Text.ToUpper();
+        }
+
+        private void clicked1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ArrayPage());
+        }
+
+        private void clicked2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ConstructorsPage());
+        }
+
+        private void clicked3(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ReferencePage());
+        }
+
+        private void clicked4(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PropertyValuesPage());
+        }
+    }
+}
